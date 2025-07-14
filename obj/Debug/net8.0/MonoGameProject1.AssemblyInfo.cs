3645438f4c2e4463e29575116e7acac2d4210430
Beru <lidor.y.shapira@gmail.com>
2025-07-15 02:33:04 +0300

    idk anymore

--- obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+030d03ada48cd4053c0551e9933d96c82a715599")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d9ab70e29084390c6cbf27b8748eac6742bb12")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
