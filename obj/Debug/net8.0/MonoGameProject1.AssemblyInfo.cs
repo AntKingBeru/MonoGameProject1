a544fd5e7d6e3d2b96c7972c26ca80ef06e8d10c
MatanBelfer <belfer.matan@gmail.com>
2025-08-14 23:20:59 +0300

    pushignore

--- obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c1bf10cb527ec685a87aecd3d73c6d08435081e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd76c804850deeaa3888d4557c29978b019a9117")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
