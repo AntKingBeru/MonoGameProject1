cd76c804850deeaa3888d4557c29978b019a9117
MatanBelfer <belfer.matan@gmail.com>
2025-08-14 23:20:11 +0300

    Revert "testpush"
    This reverts commit adbe7bd93aa31999bbb392f8b2b89adec1bc9a6f.

--- obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b372666b4680e81fc30e3ad9ecb25a9a9bf95583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c1bf10cb527ec685a87aecd3d73c6d08435081e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
