8b0ca4ba7ed7d6ec35270201e1e610515bdb7476
MatanBelfer <belfer.matan@gmail.com>
2025-08-14 23:19:49 +0300

    Revert "commit"
    This reverts commit ebb25a76ffb52f3e89f2fb82ccf06e59a3755a88.

--- obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonoGameProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbe7bd93aa31999bbb392f8b2b89adec1bc9a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b372666b4680e81fc30e3ad9ecb25a9a9bf95583")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
