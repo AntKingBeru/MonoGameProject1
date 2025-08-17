5a35fc4b8c90ef6c81a68396d9da98c1af426091
MatanBelfer <belfer.matan@gmail.com>
2025-08-17 11:13:59 +0300

    push

--- MonoGameProject1/Core/FishPatterns.cs
+++ MonoGameProject1/Core/FishPatterns.cs
@@ -5,36 +5,37 @@ namespace MonoGameProject1.Core;
 
 public static class FishPatterns
 {
-    public static Func<GameObject, GameTime, Vector2> SineWave = (gameObject, gameTime) =>
+    public static Vector2 SineWave(GameObject go, GameTime gt)
     {
-        float time = (float)gameTime.TotalGameTime.TotalSeconds;
+        float time = (float)gt.TotalGameTime.TotalSeconds;
         float amplitude = 100f;
         float frequency = 2f;
-        
+
         return new Vector2(
-            gameObject.Position.X + MathF.Sin(time * frequency) * amplitude,
-            gameObject.Position.Y
+            go.Position.X + MathF.Sin(time * frequency) * amplitude,
+            go.Position.Y
         );
-    };
-    
-    public static Func<GameObject, GameTime, Vector2> ZigZag = (gameObject, gameTime) =>
+    }
+
+    public static Vector2 ZigZag(GameObject go, GameTime gt)
     {
-        float time = (float)gameTime.TotalGameTime.TotalSeconds;
+        float time = (float)gt.TotalGameTime.TotalSeconds;
         float speed = 100f;
         float width = 150f;
-        
-        float x = gameObject.Position.X + (time % 2 < 1 ? speed : -speed) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        float x = go.Position.X +
+                  (time % 2 < 1 ? speed : -speed) * (float)gt.ElapsedGameTime.TotalSeconds;
         x = MathHelper.Clamp(x, -width, width);
-        
-        return new Vector2(x, gameObject.Position.Y);
-    };
-    
+
+        return new Vector2(x, go.Position.Y);
+    }
+
     public static Vector2 Circle(GameObject go, GameTime gt)
     {
         float time = (float)gt.TotalGameTime.TotalSeconds;
         float radius = 5f;
         float speed = 2f;
-        
+
         return new Vector2(
             go.Position.X + MathF.Cos(time * speed) * radius,
             go.Position.Y + MathF.Sin(time * speed) * radius
