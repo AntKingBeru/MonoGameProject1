f8144e99f14adc12249a8ebf3a1e3d3c58f4abd6
MatanBelfer <belfer.matan@gmail.com>
2025-08-19 02:48:43 +0300

    push

--- MonoGameProject1/Scenes/EndScene.cs
+++ MonoGameProject1/Scenes/EndScene.cs
@@ -8,8 +8,8 @@ public class EndScene : Scene
         
         var exitButton = new ExitButton("Exit");
         AddActiveObject(exitButton);
-        // var restartButton = new RestartButton("Restart");
-        // AddActiveObject(restartButton);
+        var restartButton = new RestartButton("Restart");
+        AddActiveObject(restartButton);
         var endTitle = new EndTitle("EndTitle");
         AddActiveObject(endTitle);
         var endScore = new EndScore("EndScore");
