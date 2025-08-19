73d46e70c9f72bc605064fc28f06632d670b2787
TheFull0 <joester17@gmail.com>
2025-08-19 06:18:24 +0300

    finalizing finalizations
    thank you for bearing in there and pulling through this tough assignment. im proud of us and happy we got to work together.

--- MonoGameProject1/Game/Game1.cs
+++ MonoGameProject1/Game/Game1.cs
@@ -22,8 +22,7 @@ public class Game1 : Game
 
         ManagersContentRegistration();
     }
-
-
+    
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
@@ -78,12 +77,16 @@ public class Game1 : Game
         AudioManager.RegisterSFX("Spear","Audio/SFX/Spearthrow");
         AudioManager.RegisterSFX("Splat1","Audio/SFX/SplatSound");
         AudioManager.RegisterSFX("Splat2","Audio/SFX/SplatSound2");
+        
         AudioManager.RegisterSong("Theme","Audio/Music/Theme");
         AudioManager.RegisterSong("InGameTheme","Audio/Music/MarioSong");
+        
         
         TextManager.ContentMan = Content;
         TextManager.AddFont("Oswald", "Fonts/Oswald");
         TextManager.AddFont("Baloo2", "Fonts/Baloo2-SemiBold");
+        
+        
         SpriteManager.ContentMan = Content;
         SpriteManager.AddSprite("Button", "Images/pacman");
         SpriteManager.AddSprite("ExitButton", "Images/Exit");
@@ -108,5 +111,4 @@ public class Game1 : Game
         SpriteManager.AddSprite("BoostBar", "Images/BoostBar");
         SpriteManager.AddSprite("TitleImage", "Images/TitleImage");
     }
-
 }
